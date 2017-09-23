2bea539637fd4692a84d4a148860b2f34ac245df
Daniil Boyko <daniil.boyko.2017@gmail.com>
2017-09-24 00:38:07 +0400

    Add functionality to class GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
+    	this.min = min;
+    	this.max = max;
+    }
 
+    middle() { 
+    	return Math.ceil((this.max + this.min)/2);
     }
 
     guess() {
-
+    	return this.middle();
     }
 
     lower() {
-
+    	this.max = this.middle();
     }
 
     greater() {
-
+    	this.min = this.middle();
     }
 }
 
